7836d225b8b1d3de2b6697419f232472a7556854
spython <emmanuelegbo01@gmail.com>
2024-04-28 08:40:30 +0100

    commit

--- src/todoForm/TodoForm.jsx
+++ src/todoForm/TodoForm.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import './todoForm.scss'
 
 export default function TodoForm() {
+ const [todoName, setTodoName] = useState('');
+
   return (
     <div className='todoForm'>
         <form>
-            <input type="text" placeholder='add an activity...'/>
+            <input 
+              type="text" 
+              placeholder='add an activity...'
+              value={todoName}
+              onChange = {e => setTodoName(e.target.value)}
+              />
             <button>+</button>
         </form>
     </div>
