8df8f0efbe1ef4991042f97bcb2dfd156a20229c
spython <emmanuelegbo01@gmail.com>
2024-05-01 05:21:27 +0100

    added a package

--- src/tasks/Tasks.jsx
+++ src/tasks/Tasks.jsx
@@ -3,7 +3,7 @@ import './tasks.scss'
 export default function Tasks({name, finish, onToggle}) {
   return (
     <div className={'task ' + (finish ?'finish': '')}>
-        <input type="checkbox" checked={finish} onChange={() => onToggle(!finish)}/>
+        <div className="inputCheck"><input type="checkbox" checked={finish} onChange={() => onToggle(!finish)}/></div>
         
         <div className="taskName">
           <span>{name}</span>
